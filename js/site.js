4774c31ad7d68318772342eaa21b5c8d2fbaac07
Anthony Martinez <anthony@anthonymartinez.io>
2021-09-16 17:42:53 -0700

    JS Primary Btn

--- js/site.js
+++ js/site.js
@@ -1,3 +1,12 @@
 let primaryBTN = document.querySelector(".primary-btn");
+let primaryButtonIcon = document.querySelector(".icon-btn");
 
-primaryBTN.
+primaryBTN.onmouseover = function() {
+   this.style.backgroundColor = "#5FB4A2";
+   primaryButtonIcon.style.backgroundColor = "#56A292";
+}
+
+primaryBTN.onmouseout = function() {
+   this.style.backgroundColor = "#203A4C";
+   primaryButtonIcon.style.backgroundColor = "#1D3445";
+}
